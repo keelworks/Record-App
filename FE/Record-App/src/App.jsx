a405608f96d6b474aa2e092e744a4f6a8d96c028
kapilgupta716 <113945662+kapilgupta716@users.noreply.github.com>
2024-06-09 13:38:34 -0500

    Update for participant profile

--- FE/Record-App/src/App.jsx
+++ FE/Record-App/src/App.jsx
@@ -4,14 +4,28 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import LoginPage from './component/LoginPage'
 import HomePage from './component/HomePage/HomePage'
-
+import Main from './component/HomePage/Main'
+import MainPage from './component/MainPage'
+import ParticipantProfile from './component/ParticipantProfile'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <BrowserRouter>
+
+    <Routes>
+        <Route path="/login" element={<LoginPage/>} />
+        <Route path="/home" element={<HomePage />} /> 
+        <Route path="/main" element={<MainPage />} />
+        <Route path="/profile" element={<ParticipantProfile />} />
+    </Routes>
+
+    </BrowserRouter>
       {/* <LoginPage/> */}
-      <HomePage/>
+
+      
     </>
   )
 }
